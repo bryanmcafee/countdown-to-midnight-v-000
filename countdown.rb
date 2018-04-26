b3c9ca0506d85a88d25b2fb8c6f08f448572b32a
Bryan McAfee <brymcafee@gmail.com>
2018-04-26 13:52:35 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(seconds)
   return "HAPPY NEW YEAR!"
 
 end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECONDS(S)"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+
+end
